a9a48817e569283c3788170755ba1c86ced2024e
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2020-12-20 20:54:13 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+import  java.util.Scanner ;
+
+class  Main {
+  public  static  void  main ( String [] args ) {
+    Entrada del escáner = nuevo  escáner ( System . In);
+    Sistema . fuera . println ( " Introduzca un número " );
+    int valor = entrada . nextInt ();
+    boolean resul = espositivo (valor);
+    si (resultado)
+        Sistema . fuera . println ( " Es positivo " );
+    más
+        Sistema . fuera . println ( " Es negativo " );
+  }
+
+  public  static  boolean  esPositivo ( int  nro ) {
+      si (nro > 0 )
+        devuelve  verdadero ;
+      más
+        devolver  falso ;
+
+  }
+}
