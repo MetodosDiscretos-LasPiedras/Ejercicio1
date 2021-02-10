3d9f2c28d66004469007fb2454d15c0b84ed73e2
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2021-02-09 22:25:09 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,11 @@
-
-import  java.util.Scanner ;
-
-class  Main {
-  public  static  void  main ( String [] args ) {
-    Entrada del escáner = nuevo  escáner ( System . In);
-    Sistema . fuera . println ( " Introduzca un número " );
-    int valor = entrada . nextInt ();
-    boolean resul = espositivo (valor);
-    si (resultado)
-        Sistema . fuera . println ( " Es positivo " );
-    más
-        Sistema . fuera . println ( " Es negativo " );
+public class Main {
+  public static void main(String[] args) {
+    double resul=inverso(0);
+    System.out.println("El cuadrado es: "+resul);
   }
-
-  public  static  boolean  esPositivo ( int  nro ) {
-      si (nro > 0 )
-        devuelve  verdadero ;
-      más
-        devolver  falso ;
-
-  }
-}
+  
+ public static int cuadrado(int nro1 ){
+   
+   return nro1*nro1;
+   
+ }
